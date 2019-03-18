fe60ddfedfd58b00c257a611f70e0ff162d1213b
Daniele Pignedoli <daniele.pingedoli@studio.unibo.it>
2019-03-18 19:49:04 +0100

    nuovo pigreco

--- pigreco.py
+++ pigreco.py
@@ -12,18 +12,20 @@ import time
 N = 100000 # number of MC events
 N_run = 100 # number of runs
 #Nhits = 0.0 # number of points accepted
-pi = np.zeros(N_run) # values of pi
+#pi = np.zeros(N_run) # values of pi
 
 start_time = time.time() # start clock 
 
-for I in range(N_run):
-    res = (np.random.rand(N)*2-1)**2+(np.random.rand(N)*2-1)**2
-    pi[I] += 4. * np.count_nonzero(np.where(res<1,res,0))/N
+
+#for I in range(N_run):
+ #   res = (np.random.rand(N)*2-1)**2+(np.random.rand(N)*2-1)**2
+  #  pi[I] += 4. * np.count_nonzero(np.where(res<1,res,0))/N
+pi=np.mean(np.mean(np.where(np.sum(pow(np.random.rand(N_run,2,N),2),axis=1)<1,1,0),axis=0)*4)
 
 run_time = time.time()
 
-print("pi with ", N, " steps for ", N_run, " runs is ", np.mean(pi), " in ", run_time-start_time, " sec")
-print("Precision computation : ", np.abs(np.mean(pi)-np.pi))
+print("pi with ", N, " steps for ", N_run, " runs is ", pi, " in ", run_time-start_time, " sec")
+print("Precision computation : ", np.abs(pi-np.pi))
 
 
 
